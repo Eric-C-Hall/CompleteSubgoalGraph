f5327418fc5c5da900b373f428eaeba41e534c29
Eric  Hall <eric.hall@anu.edu.au>
2021-05-19 13:25:18 +1000

    Moving templates to more appropriate place

--- src/Run/GetPath/GetPath.cpp
+++ src/Run/GetPath/GetPath.cpp
@@ -115,47 +115,4 @@ bool PreprocessingData::get_path(map_position start, map_position goal, std::vec
   return true;
 }
 
-// Doesn't add start to path, but does add end.
-template<bool x_diff_greater_than_y_diff, bool test_valid>
-void PreprocessingData::_compute_diagonal_then_straight_path(xyLoc start, int16_t num_diagonal, int16_t num_straight, int16_t x_step, int16_t y_step, std::vector<xyLoc> &path) const
-{
-  while(num_diagonal > 0)
-  {
-    start.x += x_step;
-    start.y += y_step;
-    if (test_valid)
-      if (graph.is_obstacle(graph.pos(start.x, start.y)))
-        return;
-    path.push_back(start);
-    num_diagonal--;
-  }
-  while(num_straight > 0)
-  {
-    if (x_diff_greater_than_y_diff)
-      start.x += x_step;
-    else
-      start.y += y_step;
-    if (test_valid)
-      if (graph.is_obstacle(graph.pos(start.x, start.y)))
-        return;
-    path.push_back(start);
-    num_straight--;
-  }
-}
-
-// Doesn't add start to path, but does add end.
-template<bool test_valid>
-void PreprocessingData::_compute_octile_path(xyLoc start, xyLoc end, std::vector<xyLoc> &path) const
-{
-  int16_t x_diff = std::abs(start.x - end.x);
-  int16_t y_diff = std::abs(start.y - end.y);
-  int16_t x_step = (start.x > end.x ? -1 : 1);
-  int16_t y_step = (start.y > end.y ? -1 : 1);
-
-  if (x_diff > y_diff)
-    _compute_diagonal_then_straight_path<true, test_valid>(start, y_diff, x_diff - y_diff, x_step, y_step, path);
-  else
-    _compute_diagonal_then_straight_path<false, test_valid>(start, x_diff, y_diff - x_diff, x_step, y_step, path);
-}
-
 // eof
