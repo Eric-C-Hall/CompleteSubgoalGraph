52548aeb4ad850922513ed66373a5592c43ac0c5
Eric  Hall <eric.hall@anu.edu.au>
2021-06-10 12:17:58 +1000

    Adding functions for choosing which parts of path computation to do

--- src/Preprocess/Preprocess.hpp
+++ src/Preprocess/Preprocess.hpp
@@ -89,7 +89,9 @@ class PreprocessingData {
   template<bool try_octile, bool test_double, bool test_single, bool compute_path>
   bool get_path_partial_computation(map_position start, map_position goal, std::vector<xyLoc> &path) const;
   public:
-  //bool get_path_octile_step_only(map_position start, map_position goal, std::vector<xyLoc> &path) const;
+  bool get_path_octile_step_only(map_position start, map_position goal, std::vector<xyLoc> &path) const;
+  bool get_path_up_to_double_step(map_position start, map_position goal, std::vector<xyLoc> &path) const;
+  bool get_path_up_to_single_step(map_position start, map_position goal, std::vector<xyLoc> &path) const;
   bool get_path(map_position start, map_position goal, std::vector<xyLoc> &path) const;
   //-------------------------
   
--- src/Run/GetPath/GetPath.cpp
+++ src/Run/GetPath/GetPath.cpp
@@ -167,6 +167,21 @@ bool PreprocessingData::get_path_partial_computation(map_position start, map_pos
   }
 }
 
+bool PreprocessingData::get_path_octile_step_only(map_position start, map_position goal, std::vector<xyLoc> &path) const
+{
+  return get_path_partial_computation<true,false,false,false>(start, goal, path);
+}
+
+bool PreprocessingData::get_path_up_to_double_step(map_position start, map_position goal, std::vector<xyLoc> &path) const
+{
+  return get_path_partial_computation<true,true,false,false>(start, goal, path);
+}
+
+bool PreprocessingData::get_path_up_to_single_step(map_position start, map_position goal, std::vector<xyLoc> &path) const
+{
+  return get_path_partial_computation<true,true,true,false>(start, goal, path);
+}
+
 bool PreprocessingData::get_path(map_position start, map_position goal, std::vector<xyLoc> &path) const
 {
   return get_path_partial_computation<true,true,true,true>(start, goal, path);
